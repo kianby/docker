733f604fd21f262977e90d3677a6c6394976ce9e
Yax <kianby@madyanne.fr>
2018-06-16 19:58:57 +0200

    rabbitmq image

---
+++ image/rabbit/docker-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# start
+export RABBITMQ_PID_FILE=/tmp/rabbitmq.pid
+rabbitmq-server &
+rabbitmqctl wait /tmp/rabbitmq.pid -t 20
+
+# configure
+rabbitmqctl add_vhost $RABBITMQ_APP_VHOST
+rabbitmqctl add_user $RABBITMQ_APP_USER $RABBITMQ_APP_PASSWORD
+rabbitmqctl set_permissions -p $RABBITMQ_APP_VHOST $RABBITMQ_APP_USER ".*" ".*" ".*"
+
+# infinite tail 
+tail -f /dev/null
