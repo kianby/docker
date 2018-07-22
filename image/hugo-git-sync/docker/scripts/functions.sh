4d167bf878e03ff92aaa1beb1e8aa5d8be00de4d
Yax <kianby@madyanne.fr>
2018-07-22 13:28:11 +0200

    remove cleanDestinationDir

--- image/hugo-git-sync/docker/scripts/functions.sh
+++ image/hugo-git-sync/docker/scripts/functions.sh
@@ -4,5 +4,5 @@ Update() {
   rm -rf /tmp/hugo_website
   git clone --recursive $REPO_LINK /tmp/hugo_website
   cd /tmp/hugo_website
-  hugo --cleanDestinationDir --ignoreCache --destination /var/www/html --config=$CONFIG_FILE
+  hugo --ignoreCache --destination /var/www/html --config=$CONFIG_FILE
 }
