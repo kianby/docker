ec410d2e52ef2da08b41a897a7401e1b949bdd17
Yax <kianby@madyanne.fr>
2018-10-09 19:09:10 +0200

    improve hugo git sync

--- image/hugo-git-sync/docker/scripts/functions.sh
+++ image/hugo-git-sync/docker/scripts/functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Update() {
-  rm -rf /tmp/hugo_website
+  rm -rf /tmp/hugo_website/*
   git clone --recursive $REPO_LINK /tmp/hugo_website
   cd /tmp/hugo_website
   hugo --ignoreCache --destination /var/www/html --config=$CONFIG_FILE
