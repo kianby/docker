7473b4075892385d35f7ce457d28c4993cdaf571
Yax <kianby@madyanne.fr>
2018-11-04 14:37:00 +0100

    add backup options

--- image/backuper/backup.sh
+++ image/backuper/backup.sh
@@ -4,7 +4,7 @@ echo "user $MAILX_SMTP_USER host $MAILX_SMTP_HOST admin $MAILX_SMTP_TOADMIN"
 # build backup
 [ ! -d /target ] && mkdir -p /target
 [ -f /target/backup.tar.gz ] && rm -f /target/backup.tar.gz
-tar cvpzf /target/backup.tar.gz /source
+tar cvpzf /target/backup.tar.gz $BACKUP_OPTIONS /source
 
 # mail backup
 cd /target
